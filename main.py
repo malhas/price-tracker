b2f65f683b779c92068372d36815fcede8fc82c3
lmalheiro <luismalheiro1986@gmail.com>
2022-12-27 11:37:48 +0000

    some code cleanup added popup option to csv

--- main.py
+++ main.py
@@ -15,10 +15,25 @@ import os
 OWN_EMAIL = os.environ['SENDER_EMAIL']
 OWN_PASSWORD = os.environ['SENDER_EMAIL_PASSWORD']
 
+SLEEP_TIME = 2
+TODAY = datetime.now().date().strftime("%d-%m-%Y")
+
+# Selenium Options
+OPTIONS = Options()
+OPTIONS.add_argument("--no-sandbox")
+OPTIONS.add_argument("--headless")
+OPTIONS.add_argument("--disable-gpu")
+OPTIONS.add_argument('--disable-dev-shm-usage')
+
 
 def price_str_to_float(price_euro:str) -> float:
     '''Convert string price to float'''
-    return float(price_euro.replace("€", "").replace(",", "."))
+    try:
+        price = float(price_euro.replace("€", "").replace(",", "."))
+    except ValueError:
+        price = 0
+
+    return price
 
 
 def send_email(product: str, email: str, message: str, price: float):
@@ -31,73 +46,65 @@ def send_email(product: str, email: str, message: str, price: float):
         connection.sendmail(OWN_EMAIL, email.split(";"), email_message)
 
 
-data = pandas.read_csv("products.csv")
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument('--disable-dev-shm-usage')
-try:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                              options=options)
-except SessionNotCreatedException:
-    driver = webdriver.Chrome(executable_path="chromedriver", options=options)
-except WebDriverException:
-    driver = webdriver.Chrome(options=options)
-today = datetime.now().date().strftime("%d-%m-%Y")
-for index, product in data.iterrows():
-    price = 0
-    print(product["item"])
-    driver.get(product.link)
-    time.sleep(2)
-    if product.type == "id":
-        price = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.ID, product.location)))
-        price = price_str_to_float(price.text)
-    elif product.type == "class":
-        price = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.CLASS_NAME, product.location)))
-        price = price_str_to_float(price.text)
-    elif product.type == "xpath":
-        price = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.XPATH, product.location)))
-        price = price_str_to_float(price.text)
-    elif product.type == "css":
-        price = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.CSS_SELECTOR, product.location)))
-        price = price_str_to_float(price.text)
-    elif product.type == "lionofporches":
-        WebDriverWait(driver, 10).until(
-            ec.element_to_be_clickable(
-                (By.XPATH, '//*[@id="popup-campaign"]/div[2]'))).click()
+def get_price(locator, product) -> float:
+    '''Get current price from product'''
+    if str(product.popup) != 'nan':
+        WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, product.popup))).click()
         time.sleep(2)
-        items = WebDriverWait(driver, 10).until(
-            ec.presence_of_all_elements_located((By.CLASS_NAME, product.location)))
-        if len(items) > 0:
-            i = 0
-            for item in items:
-                item_price = price_str_to_float(item.text)
-                if item_price < price and i > 0 or i == 0:
-                    price = item_price
-    if price > 0:
-        last_price = 0
-        try:
-            with open(f"items/{product['item']}.csv", "r") as file:
-                lines = file.readlines()
-        except FileNotFoundError:
-            with open(f"items/{product['item']}.csv", "w") as file:
-                file.write(f"Date,Price\n{today},{price}\n")
-        else:
-            if len(lines) > 0:
-                item_data = pandas.read_csv(f"items/{product['item']}.csv")
-                last_price = item_data.iloc[-1]["Price"]
-                if item_data.Date[len(item_data)-1] != today:
-                    item_data.loc[len(item_data)] = [today,price]
-                elif item_data.Price[len(item_data)-1] != price:
-                    item_data.loc[len(item_data)-1] = [today,price]
-                item_data.to_csv(f"items/{product['item']}.csv", index=False)
-            else:
+    items = WebDriverWait(driver, 10).until(ec.presence_of_all_elements_located((locator, product.location)))
+    price = 0
+    i = 0
+    for item in items:
+        if item.text != "":
+            item_price = price_str_to_float(item.text)
+            if item_price < price and i > 0 or i == 0:
+                price = item_price
+
+    return price
+
+
+if __name__ == "__main__":
+    data = pandas.read_csv("products.csv")
+    try:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=OPTIONS)
+    except SessionNotCreatedException:
+        driver = webdriver.Chrome(executable_path="chromedriver", options=OPTIONS)
+    except WebDriverException:
+        driver = webdriver.Chrome(options=OPTIONS)
+    for index, product in data.iterrows():
+        price = 0
+        print(product["item"], product["link"])
+        driver.get(product.link)
+        time.sleep(SLEEP_TIME)
+        if product.type == "id":
+            product_locator = By.ID
+        elif product.type == "class":
+            product_locator = By.CLASS_NAME
+        elif product.type == "xpath":
+            product_locator = By.XPATH
+        elif product.type == "css":
+            product_locator = By.CSS_SELECTOR
+
+        price = get_price(product_locator, product)
+        if price > 0:
+            last_price = 0
+            try:
+                with open(f"items/{product['item']}.csv", "r") as file:
+                    lines = file.readlines()
+            except FileNotFoundError:
                 with open(f"items/{product['item']}.csv", "w") as file:
-                    file.write(f"Date,Price\n{today},{price}\n")
-        if float(product.target) >= price != last_price:
-            send_email(product['item'].title(), product.email, f"{product.link}", price)
+                    file.write(f"Date,Price\n{TODAY},{price}\n")
+            else:
+                if len(lines) > 0:
+                    item_data = pandas.read_csv(f"items/{product['item']}.csv")
+                    last_price = item_data.iloc[-1]["Price"]
+                    if item_data.Date[len(item_data)-1] != TODAY:
+                        item_data.loc[len(item_data)] = [TODAY,price]
+                    elif item_data.Price[len(item_data)-1] != price:
+                        item_data.loc[len(item_data)-1] = [TODAY,price]
+                    item_data.to_csv(f"items/{product['item']}.csv", index=False)
+                else:
+                    with open(f"items/{product['item']}.csv", "w") as file:
+                        file.write(f"Date,Price\n{TODAY},{price}\n")
+            if float(product.target) >= price != last_price:
+                send_email(product['item'].title(), product.email, f"{product.link}", price)
